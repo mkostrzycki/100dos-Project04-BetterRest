d9088d2517aed55d46449597beed493078809de3
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-04-01 16:57:21 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by MaćKo on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
